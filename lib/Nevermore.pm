def6657f2bc5d90d89be0e8c2ecce881eb93c1f4
Sebastian Riedel <kraihx@googlemail.com>
2008-09-03 17:32:48 +0000

    Import of SRI/Nevermore-0.01 from CPAN.
    gitpan-cpan-distribution: Nevermore
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SRI/Nevermore-0.01.tar.gz
    gitpan-cpan-author:       SRI
    gitpan-cpan-maturity:     released

---
+++ lib/Nevermore.pm
@@ -0,0 +1,31 @@
+package Nevermore;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 DISCLAIMER
+
+Hi there, this is just a placeholder for the Mojo framework.
+
+More information will soon be available at L<http://gotmojo.kraih.com>.
+
+I'm uploading this only to make sure i can safely blog about Mojo without the
+risk of losing the namespace to someone trying to be funny.
+
+=head1 AUTHOR
+
+Sebastian Riedel, C<sri@cpan.org>.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2008, Sebastian Riedel.
+
+This program is free software, you can redistribute it and/or modify it under
+the same terms as Perl 5.10.
+
+=cut
---
+++ t/use.t
@@ -0,0 +1,11 @@
+#!perl
+
+use strict;
+use warnings;
+
+use Test::More tests => 4;
+
+use_ok('Curse');
+use_ok('Mojo');
+use_ok('Nevermore');
+use_ok('Voodoo');
